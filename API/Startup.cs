e35c1d798bb40a1d30921d7b8e480f25ac62744b
Brian Fontana <brian@codefoxtrot.com>
2021-05-23 17:39:25 -0400

    Reconfigure CORS for BlazorClient App

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
                 options.UseSqlServer(_config.GetConnectionString("Default"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -52,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
