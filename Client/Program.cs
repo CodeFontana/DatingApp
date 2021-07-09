a5c1038fc0ef78969cfe496550efdf410cd1c157
Brian Fontana <brian@codefoxtrot.com>
2021-07-09 10:20:17 -0400

    Add login and Navbar to client

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Client.Services;
 
 namespace Client
 {
@@ -18,6 +19,7 @@ namespace Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+            builder.Services.AddHttpClient<AccountService>();
 
             await builder.Build().RunAsync();
         }
---
+++ Client/Services/AccountService.cs
@@ -0,0 +1,43 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Client.Services
+{
+    public class AccountService
+    {
+        private readonly HttpClient _httpClient;
+        public bool LoggedIn = false;
+
+        public AccountService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:5001");
+        }
+
+        public async Task Login(LoginModel loginUser)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/Account/login", loginUser);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Login successful.");
+                LoggedIn = true;
+            }
+            else
+            {
+                Console.WriteLine($"Login failed: {response.ReasonPhrase}");
+                LoggedIn = false;
+            }
+        }
+
+        public void Logout()
+        {
+            LoggedIn = false;
+        }
+    }
+}
