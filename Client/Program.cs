eb2e56ac9e17c23e5a17557be116d15e1c9606e9
Brian Fontana <brian@codefoxtrot.com>
2021-08-11 01:57:51 -0400

    Cleanup using statements.

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,15 +4,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Client.Interfaces;
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.Toast;
+using Blazored.LocalStorage;
+using Client.Interfaces;
 using Client.Authentication;
 using Client.Services;
-using Blazored.Toast;
 
 namespace Client
 {
