750fe35d05c31e8cffab34fbb6d363226b16b211
Brian Fontana <brian@codefoxtrot.com>
2021-09-20 02:01:34 -0400

    Fix 401 errors loading images before they are downloaded

--- Client/Services/ImageService.cs
+++ Client/Services/ImageService.cs
@@ -2,7 +2,6 @@
 using DataAccessLibrary.Models;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
