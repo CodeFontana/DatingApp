13b18ed6ffb892d6525fe046a7381befc64a4369
Brian Fontana <brian@codefoxtrot.com>
2021-09-24 19:05:08 -0400

    Expand user registration model and validations

--- DataAccessLibrary/Models/RegisterUserModel.cs
+++ DataAccessLibrary/Models/RegisterUserModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace DataAccessLibrary.Models
@@ -8,13 +9,33 @@ namespace DataAccessLibrary.Models
         [Required(ErrorMessage = "Please enter a username")]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "Please enter your preferred name")]
+        [MaxLength(50, ErrorMessage = "Your preferred name must be less than (50) characters")]
+        public string KnownAs { get; set; }
+
+        [Required(ErrorMessage = "Please specify your biology")]
+        [MaxLength(25)]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter your date of birth")]
+        public DateTime DateOfBirth { get; set; } = DateTime.Now.AddYears(-20);
+
+        [Required(ErrorMessage = "Please enter your town or city")]
+        [MaxLength(100, ErrorMessage = "Your town name must be less than (100) characters")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter your state")]
+        [MaxLength(100, ErrorMessage = "Your state name must be less than (100) characters")]
+        public string State { get; set; }
+
         [Required(ErrorMessage = "Please enter a password")]
         [MinLength(4, ErrorMessage="Password must be at least (4) characters")]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please confirm your password")]
         [DisplayName("Confirm Password")]
         [Compare(nameof(Password), ErrorMessage = "Password does not match.")]
         public string ConfirmPassword { get; set; }
+
     }
 }
