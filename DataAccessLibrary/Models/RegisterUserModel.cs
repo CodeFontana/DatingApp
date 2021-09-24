5e660778aa6a62f0e0050ae911abbf1ddf9167b2
Brian Fontana <brian@codefoxtrot.com>
2021-09-24 15:12:33 -0400

    Trim unnecessary using statements

--- DataAccessLibrary/Models/RegisterUserModel.cs
+++ DataAccessLibrary/Models/RegisterUserModel.cs
@@ -1,11 +1,5 @@
-﻿using DataAccessLibrary.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLibrary.Models
 {
