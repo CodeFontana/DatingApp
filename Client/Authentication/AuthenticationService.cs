f206e13a7e0cc6f6f737e069610e3acdef4f4699
Brian Fontana <brian@codefoxtrot.com>
2021-09-27 15:44:53 -0400

    Trim unnecessary usings

--- Client/Authentication/AuthenticationService.cs
+++ Client/Authentication/AuthenticationService.cs
@@ -1,13 +1,8 @@
 ﻿using Client.Interfaces;
-using DataAccessLibrary.Interfaces;
 using DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
