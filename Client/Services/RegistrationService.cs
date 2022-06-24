657ec2abc3e5ba79fbbd0be3dab5fdbfaf080d0b
Brian Fontana <brian@codefoxtrot.com>
2022-06-23 21:08:24 -0400

    Fix new user registration
    The MudRadioGroup uses @bind-SelectedOption, and not @bind-Value. This was causing the data model to be invalid, resulting in a BadRequest when registering a new user.

--- Client/Services/RegistrationService.cs
+++ Client/Services/RegistrationService.cs
@@ -1,11 +1,6 @@
-﻿using Client.Authentication;
-using Client.Interfaces;
-using DataAccessLibrary.Interfaces;
+﻿using Client.Interfaces;
 using DataAccessLibrary.Models;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
