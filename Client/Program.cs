2775668959715274c3f3b00855c88b471775c674
Brian Fontana <brian@codefoxtrot.com>
2022-08-03 17:50:30 -0400

    Set page titles

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,7 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddBlazoredLocalStorage();
         builder.Services.AddMudServices(config =>
