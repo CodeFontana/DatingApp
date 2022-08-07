adb81022f96652bc07e1d57d1bca0ccbec557800
Brian Fontana <brian@codefoxtrot.com>
2022-08-06 22:38:02 -0400

    Add MemberMessageComponent
    This is an initial commit to lay the foundation for this component

---
+++ Client/Components/MemberMessage.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace Client.Components;
+
+public partial class MemberMessage
+{
+    [Inject] IMessageService MessageService { get; set; }
+    [Inject] ISnackbar Snackbar { get; set; }
+    [Parameter] public string Username { get; set; }
+
+    private List<MessageModel> _messages = new();
+    private bool _showError = false;
+    private string _errorText;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        await LoadMessages();
+    }
+
+    private async Task LoadMessages()
+    {
+        ServiceResponseModel<IEnumerable<MessageModel>> result = await MessageService.GetMessageThreadAsync(Username);
+
+        if (result.Success)
+        {
+            _messages = result.Data.ToList();
+        }
+        else
+        {
+            _showError = true;
+            _errorText = $"Request failed: {result.Message}";
+            Snackbar.Add($"Request failed: {result.Message}", Severity.Error);
+        }
+    }
+
+}
