779048a63f00eff0b687882bb7cb47aba9cfbef5
Brian Fontana <brian@codefoxtrot.com>
2022-08-20 00:35:21 -0400

    Build out of MemberMessage component
    Continue build of UI and ability to send a message to the user

--- Client/Components/MemberMessage.razor.cs
+++ Client/Components/MemberMessage.razor.cs
@@ -2,11 +2,13 @@
 
 public partial class MemberMessage
 {
+    [Inject] IMemberStateService MemberStateService { get; set; }
     [Inject] IMessageService MessageService { get; set; }
     [Inject] ISnackbar Snackbar { get; set; }
     [Parameter] public string Username { get; set; }
 
     private List<MessageModel> _messages = new();
+    private MessageCreateModel _newMessage = new();
     private bool _showError = false;
     private string _errorText;
 
@@ -31,4 +33,22 @@ public partial class MemberMessage
         }
     }
 
+    private async Task HandleSendMessage()
+    {
+        _newMessage.RecipientUsername = Username;
+        ServiceResponseModel<MessageModel> result = await MessageService.CreateMessageAsync(_newMessage);
+
+        if (result.Success)
+        {
+            result.Data.SenderPhotoUrl = MemberStateService.MainPhoto;
+            _messages.Add(result.Data);
+            _newMessage = new();
+        }
+        else
+        {
+            _showError = true;
+            _errorText = $"Request failed: {result.Message}";
+            Snackbar.Add($"Request failed: {result.Message}", Severity.Error);
+        }
+    }
 }
