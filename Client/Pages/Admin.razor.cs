7906130789c121cc38b889a055879743bff296e5
Brian Fontana <brian@codefoxtrot.com>
2022-08-21 00:33:42 -0400

    Create user management paginated table

--- Client/Pages/Admin.razor.cs
+++ Client/Pages/Admin.razor.cs
@@ -2,14 +2,46 @@
 
 public partial class Admin
 {
-    private string _pageTitle;
+    private List<UserWithRolesModel> _users = new();
+    private PaginationParameters _pageParameters = new();
+    private PaginationModel _metaData;
+    private bool _loadingUsers = false;
     private bool _showError = false;
-    private string _errorText;
+    private string _errorText = "";
 
+    [Inject] IAdminService AdminService { get; set; }
     [Inject] ISnackbar Snackbar { get; set; }
 
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
-        _pageTitle = "Admin Panel";
+        await LoadUsersWithRolesAsync();
+    }
+
+    private async Task LoadUsersWithRolesAsync()
+    {
+        _loadingUsers = true;
+        PaginationResponseModel<IEnumerable<UserWithRolesModel>> result = await AdminService.GetUsersWithRolesAsync(_pageParameters);
+
+        if (result.Success)
+        {
+            _showError = false;
+            _users = result.Data.ToList();
+            _metaData = result.MetaData;
+        }
+        else
+        {
+            _showError = true;
+            _errorText = $"Request failed: {result.Message}";
+            Snackbar.Add($"Request failed: {result.Message}", Severity.Error);
+        }
+
+        _loadingUsers = false;
+    }
+
+    private async Task HandlePageChangedAsync(int page)
+    {
+        _pageParameters.PageNumber = page;
+        _users = null;
+        await LoadUsersWithRolesAsync();
     }
 }
