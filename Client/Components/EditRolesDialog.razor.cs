fa16d360b0ccbe14441763288f729ff4cef0f2e2
Brian Fontana <brian@codefoxtrot.com>
2022-08-21 23:13:40 -0400

    Complete Edit-User-Roles implementation

--- Client/Components/EditRolesDialog.razor.cs
+++ Client/Components/EditRolesDialog.razor.cs
@@ -2,7 +2,12 @@ namespace Client.Components;
 
 public partial class EditRolesDialog
 {
+    private string _leftValue;
+    private string _rightValue;
+    private List<string> _diffRoles;
+
     [Inject] IAdminService AdminService { get; set; }
+    [Inject] ISnackbar Snackbar { get; set; }
     [CascadingParameter] public MudDialogInstance MudDialog { get; set; }
     [Parameter] public UserWithRolesModel User { get; set; }
     [Parameter] public List<string> AvailableRoles { get; set; }
@@ -18,8 +23,47 @@ public partial class EditRolesDialog
     protected override void OnParametersSet()
     {
         MudDialog.SetTitle($"Edit roles for {User.Username}");
+        _diffRoles = AvailableRoles.Except(User.Roles).ToList();
     }
 
-    void Submit() => MudDialog.Close(DialogResult.Ok(true));
-    void Cancel() => MudDialog.Cancel();
+    private void HandleRight()
+    {
+        if (string.IsNullOrWhiteSpace(_leftValue) == false)
+        {
+            _diffRoles.Add(_leftValue);
+            User.Roles.Remove(_leftValue);
+            _leftValue = null;
+        }
+    }
+
+    private void HandleLeft()
+    {
+        if (string.IsNullOrWhiteSpace(_rightValue) == false)
+        {
+            User.Roles.Add(_rightValue);
+            _diffRoles.Remove(_rightValue);
+            _rightValue = null;
+        }
+    }
+
+    private async Task Submit()
+    {
+        ServiceResponseModel<string> result = await AdminService.EditRolesAsync(User);
+
+        if (result.Success)
+        {
+            Snackbar.Add($"Successfully editted roles for {User.Username}", Severity.Success);
+        }
+        else
+        {
+            Snackbar.Add($"Request failed: {result.Message}", Severity.Error);
+        }
+
+        MudDialog.Close(DialogResult.Ok(true));
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+    }
 }
