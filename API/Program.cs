edc34bf09a82f6b3318ed97b50c239b2c7200ed3
Brian Fontana <brian@codefoxtrot.com>
2026-02-08 17:58:54 -0500

    Use top-level statements in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -34,241 +34,229 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
 
-namespace API;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddDbContext<DataContext>(options =>
 {
-    public static async Task Main(string[] args)
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+builder.Services.AddIdentityCore<AppUser>(opt =>
+{
+    opt.Password.RequireNonAlphanumeric = false;
+})
+    .AddRoles<AppRole>()
+    .AddRoleManager<RoleManager<AppRole>>()
+    .AddSignInManager<SignInManager<AppUser>>()
+    .AddRoleValidator<RoleValidator<AppRole>>()
+    .AddEntityFrameworkStores<DataContext>();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddDbContext<DataContext>(options =>
+        options.TokenValidationParameters = new()
         {
-            options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-        });
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:JwtIssuer"),
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration.GetValue<string>("Authentication:JwtAudience"),
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(
+                    builder.Configuration.GetValue<string>("Authentication:JwtSecurityKey"))),
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.FromSeconds(5)
+        };
 
-        builder.Services.AddIdentityCore<AppUser>(opt =>
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
             {
-                opt.Password.RequireNonAlphanumeric = false;
-            })
-            .AddRoles<AppRole>()
-            .AddRoleManager<RoleManager<AppRole>>()
-            .AddSignInManager<SignInManager<AppUser>>()
-            .AddRoleValidator<RoleValidator<AppRole>>()
-            .AddEntityFrameworkStores<DataContext>();
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
 
-        builder.Services
-            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new()
+                if (string.IsNullOrWhiteSpace(accessToken) == false && path.StartsWithSegments("/hubs"))
                 {
-                    ValidateIssuer = true,
-                    ValidIssuer = builder.Configuration.GetValue<string>("Authentication:JwtIssuer"),
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration.GetValue<string>("Authentication:JwtAudience"),
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.ASCII.GetBytes(
-                            builder.Configuration.GetValue<string>("Authentication:JwtSecurityKey"))),
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.FromSeconds(5)
-                };
-
-                options.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var accessToken = context.Request.Query["access_token"];
-                        var path = context.HttpContext.Request.Path;
-
-                        if (string.IsNullOrWhiteSpace(accessToken) == false && path.StartsWithSegments("/hubs"))
-                        {
-                            context.Token = accessToken;
-                        }
-
-                        return Task.CompletedTask;
-                    }
-                };
-            });
-
-        builder.Services.AddAuthorization(config =>
-        {
-            config.AddPolicy("RequireAdminRole", policy =>
-            {
-                policy.RequireRole("Administrator");
-            });
-
-            config.AddPolicy("ModeratePhotoRole", policy =>
-            {
-                policy.RequireRole("Moderator");
-            });
-
-            config.FallbackPolicy = new AuthorizationPolicyBuilder()
-                .RequireAuthenticatedUser()
-                .Build();
-        });
-
-        builder.Logging.ClearProviders();
-        builder.Logging.AddConsoleLogger(builder.Configuration);
-
-        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-        builder.Services.AddScoped<ITokenService, TokenService>();
-        builder.Services.AddScoped<IAccountService, AccountService>();
-        builder.Services.AddScoped<IAdminService, AdminService>();
-        builder.Services.AddScoped<IMemberService, MemberService>();
-        builder.Services.AddScoped<IPhotoService, PhotoService>();
-        builder.Services.AddScoped<ILikesService, LikesService>();
-        builder.Services.AddScoped<IMessageService, MessageService>();
-        builder.Services.AddScoped<UserActivity>();
-
-        builder.Services.AddSignalR();
-        builder.Services.AddSingleton<IUserIdProvider, NameUserIdProvider>();
-        builder.Services.AddSingleton<IPresenceTrackerService, PresenceTrackerService>();
-
-        builder.Services.AddMemoryCache();
-        builder.Services.AddResponseCaching();
-        builder.Services.AddControllers().AddJsonOptions(config =>
-        {
-            config.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-        });
-        builder.Services.AddOpenApi(options =>
-        {
-            options.AddDocumentTransformer<JwtBearerSecuritySchemeTransformer>();
-        });
-        builder.Services.AddCors(policy =>
-        {
-            policy.AddPolicy("OpenCorsPolicy", options =>
-                options.AllowAnyOrigin()
-                       .AllowAnyHeader()
-                       .AllowAnyMethod());
-
-            policy.AddPolicy("SignalRPolicy", options =>
-                options.AllowAnyHeader()
-                       .AllowAnyMethod()
-                       .AllowCredentials()
-                       .SetIsOriginAllowed(isOriginAllowed => true));
-        });
-
-        builder.Services.AddHealthChecks()
-                        .AddDbContextCheck<DataContext>("Database Health Check");
-
-        builder.Services.AddRateLimiter(options =>
-        {
-            options.AddFixedWindowLimiter("fixed", limiterOptions =>
-            {
-                limiterOptions.PermitLimit = 4;
-                limiterOptions.Window = TimeSpan.FromSeconds(12);
-                limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-                limiterOptions.QueueLimit = 0;
-            });
-
-            options.OnRejected = (context, cancellationToken) =>
-            {
-                context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
-                context.HttpContext.Response.ContentType = MediaTypeNames.Text.Plain;
-                context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>()?
-                    .CreateLogger("Microsoft.AspNetCore.RateLimitingMiddleware")
-                    .LogWarning("OnRejected: {GetUserEndPoint}", GetUserEndPoint(context.HttpContext));
-                context.HttpContext.Response.WriteAsync("Rate limit exceeded. Please try again later.", cancellationToken: cancellationToken);
-                return new ValueTask();
-            };
-        });
-
-        WebApplication app = builder.Build();
-        await ApplyDbMigrations(app);
-
-        app.UseMiddleware<ExceptionMiddleware>();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi().AllowAnonymous();
-            app.UseSwaggerUI(options =>
-            {
-                // options.SwaggerEndpoint("/openapi/v2.json", "DatingApp v2");
-                options.SwaggerEndpoint("/openapi/v1.json", "DatingApp v1");
-                options.EnableTryItOutByDefault();
-                options.ConfigObject.AdditionalItems["syntaxHighlight"] = new Dictionary<string, object>
-                {
-                    ["activated"] = false
-                };
-            });
-        }
-
-        app.UseHttpsRedirection();
-        app.UseCors("OpenCorsPolicy");
-        app.UseCors("SignalRPolicy");
-        app.UseRateLimiter();
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.UseResponseCaching();
-        app.MapControllers();
-        app.MapHub<PresenceHub>("/hubs/presence");
-        app.MapHub<MessageHub>("/hubs/message");
-        app.MapHealthChecks("/health").AllowAnonymous();
-        app.Run();
-    }
-
-    static string GetUserEndPoint(HttpContext context) =>
-        $"User {context.User.Identity?.Name ?? "Anonymous"}, " +
-        $"Endpoint: {context.Request.Path}, " +
-        $"IP: {context.Connection.RemoteIpAddress}";
-
-    public static async Task ApplyDbMigrations(WebApplication app)
-    {
-        using IServiceScope scope = app.Services.CreateScope();
-        IServiceProvider services = scope.ServiceProvider;
-
-        try
-        {
-            DataContext context = services.GetRequiredService<DataContext>();
-            ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
-            IUnitOfWork unitOfWork = services.GetRequiredService<IUnitOfWork>();
-            await context.Database.MigrateAsync();
-            await SeedData.SeedUsersAsync(logger, unitOfWork);
-            await SeedData.SeedUserLikesAndMessages(logger, unitOfWork);
-        }
-        catch (Exception ex)
-        {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred during migration.");
-            Console.ReadKey();
-        }
-    }
-
-    internal sealed class JwtBearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
-    {
-        public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
-        {
-            IEnumerable<AuthenticationScheme> authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
-
-            if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
-            {
-                // Add the security scheme at the document level
-                Dictionary<string, IOpenApiSecurityScheme> securitySchemes = new()
-                {
-                    ["Bearer"] = new OpenApiSecurityScheme
-                    {
-                        Name = "Authorization",
-                        Type = SecuritySchemeType.Http,
-                        Scheme = "bearer",
-                        BearerFormat = "JWT",
-                        In = ParameterLocation.Header,
-                        Description = "JWT Authorization header using the Bearer scheme. Enter your token (without the 'Bearer ' prefix).",
-                    }
-                };
-                document.Components ??= new OpenApiComponents();
-                document.Components.SecuritySchemes = securitySchemes;
-
-                // Apply it as a requirement for all operations
-                foreach (KeyValuePair<HttpMethod, OpenApiOperation> operation in document.Paths.Values.SelectMany(path => path.Operations!))
-                {
-                    operation.Value.Security ??= [];
-                    operation.Value.Security.Add(new OpenApiSecurityRequirement
-                    {
-                        [new OpenApiSecuritySchemeReference("Bearer", document)] = []
-                    });
+                    context.Token = accessToken;
                 }
+
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("RequireAdminRole", policy =>
+    {
+        policy.RequireRole("Administrator");
+    })
+    .AddPolicy("ModeratePhotoRole", policy =>
+    {
+        policy.RequireRole("Moderator");
+    })
+    .SetFallbackPolicy(new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build());
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsoleLogger(builder.Configuration);
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IAdminService, AdminService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+builder.Services.AddScoped<ILikesService, LikesService>();
+builder.Services.AddScoped<IMessageService, MessageService>();
+builder.Services.AddScoped<UserActivity>();
+
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<IUserIdProvider, NameUserIdProvider>();
+builder.Services.AddSingleton<IPresenceTrackerService, PresenceTrackerService>();
+
+builder.Services.AddMemoryCache();
+builder.Services.AddResponseCaching();
+builder.Services.AddControllers().AddJsonOptions(config =>
+{
+    config.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer<JwtBearerSecuritySchemeTransformer>();
+});
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("OpenCorsPolicy", options =>
+        options.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod());
+
+    policy.AddPolicy("SignalRPolicy", options =>
+        options.AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials()
+               .SetIsOriginAllowed(isOriginAllowed => true));
+});
+
+builder.Services.AddHealthChecks()
+                .AddDbContextCheck<DataContext>("Database Health Check");
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 4;
+        limiterOptions.Window = TimeSpan.FromSeconds(12);
+        limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        limiterOptions.QueueLimit = 0;
+    });
+
+    options.OnRejected = (context, cancellationToken) =>
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.ContentType = MediaTypeNames.Text.Plain;
+        context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>()?
+        .CreateLogger("Microsoft.AspNetCore.RateLimitingMiddleware")
+        .LogWarning("OnRejected: {GetUserEndPoint}", GetUserEndPoint(context.HttpContext));
+        context.HttpContext.Response.WriteAsync("Rate limit exceeded. Please try again later.", cancellationToken: cancellationToken);
+        return new ValueTask();
+    };
+});
+
+WebApplication app = builder.Build();
+await ApplyDbMigrations(app);
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi().AllowAnonymous();
+    app.UseSwaggerUI(options =>
+    {
+        // options.SwaggerEndpoint("/openapi/v2.json", "DatingApp v2");
+        options.SwaggerEndpoint("/openapi/v1.json", "DatingApp v1");
+        options.EnableTryItOutByDefault();
+        options.ConfigObject.AdditionalItems["syntaxHighlight"] = new Dictionary<string, object>
+        {
+            ["activated"] = false
+        };
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseCors("OpenCorsPolicy");
+app.UseCors("SignalRPolicy");
+app.UseRateLimiter();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseResponseCaching();
+app.MapControllers();
+app.MapHub<PresenceHub>("/hubs/presence");
+app.MapHub<MessageHub>("/hubs/message");
+app.MapHealthChecks("/health").AllowAnonymous();
+app.Run();
+
+static string GetUserEndPoint(HttpContext context) =>
+    $"User {context.User.Identity?.Name ?? "Anonymous"}, " +
+    $"Endpoint: {context.Request.Path}, " +
+    $"IP: {context.Connection.RemoteIpAddress}";
+
+static async Task ApplyDbMigrations(WebApplication app)
+{
+    using IServiceScope scope = app.Services.CreateScope();
+    IServiceProvider services = scope.ServiceProvider;
+
+    try
+    {
+        DataContext context = services.GetRequiredService<DataContext>();
+        ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+        IUnitOfWork unitOfWork = services.GetRequiredService<IUnitOfWork>();
+        await context.Database.MigrateAsync();
+        await SeedData.SeedUsersAsync(logger, unitOfWork);
+        await SeedData.SeedUserLikesAndMessages(logger, unitOfWork);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred during migration.");
+        Console.ReadKey();
+    }
+}
+
+internal sealed class JwtBearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        IEnumerable<AuthenticationScheme> authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+
+        if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
+        {
+            // Add the security scheme at the document level
+            Dictionary<string, IOpenApiSecurityScheme> securitySchemes = new()
+            {
+                ["Bearer"] = new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. Enter your token (without the 'Bearer ' prefix).",
+                }
+            };
+            document.Components ??= new OpenApiComponents();
+            document.Components.SecuritySchemes = securitySchemes;
+
+            // Apply it as a requirement for all operations
+            foreach (KeyValuePair<HttpMethod, OpenApiOperation> operation in document.Paths.Values.SelectMany(path => path.Operations!))
+            {
+                operation.Value.Security ??= [];
+                operation.Value.Security.Add(new OpenApiSecurityRequirement
+                {
+                    [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+                });
             }
         }
     }
